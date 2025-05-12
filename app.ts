ce187c429e6eb6d68504ed756776a0a13e9e210d
Techwin Labs LLP <techwinlabs@gmail.com>
2025-05-12 12:04:15 +0530

    feat:enabled static folder serve

--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
-import { connectToDB } from "./src/utils/helper.js";
 import { errorMiddleware } from "./src/middleware/error.middleware.js";
 import router from "./src/routes/index.js";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/api/v1", router);
+
+app.use(
+  "/uploads",
+  express.static(path.resolve(path.join(__dirname, "../src/uploads")))
+);
 
 app.use(errorMiddleware);
 
