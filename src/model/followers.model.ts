ec1934603e94712e77e9624913e855767d9012fc
Techwin Labs LLP <techwinlabs@gmail.com>
2025-07-07 16:18:10 +0530

    Followers model updated

--- src/model/followers.model.ts
+++ src/model/followers.model.ts
@@ -3,8 +3,8 @@ import { Schema, model } from "mongoose";
 const followersSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "user", required: true },
-    follower: { type: Schema.Types.ObjectId, ref: "user", required: true },
-    customer: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    follower: { type: Schema.Types.ObjectId, ref: "user" },
+    customer: { type: Schema.Types.ObjectId, ref: "user" },
   },
   { timestamps: true }
 );
